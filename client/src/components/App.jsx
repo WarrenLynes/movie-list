9421af04fea88413a1692a796ffd2582508ba4a4
WarrenLynes <warrenlynes@gmail.com>
2023-06-12 09:39:10 -0600

    reset to base app to branch off from for practice

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,4 @@
 import React, { useState } from 'react';
-import { useSelector, useDispatch } from "react-redux";
-import { handleFetchLists, setList } from '../actions/lists.js';
-import { handleAuthSuccess } from '../actions/auth.js';
-import { fetchLists } from '../util/api.js';
-import AddMovie from './AddMovie.jsx';
-import MovieLists from './MovieLists.jsx';
-import MoviesList from './MoviesList.jsx';
-import Form from './Form.jsx';
-import Search from './Search.jsx';
 
 const MOVIES = [
   { title: 'Mean Girls', watched: true },
@@ -18,75 +9,14 @@ const MOVIES = [
 ];
 
 
-const App = ({ movieData, requestToken, sessionId }) => {
-  const { lists, list } = useSelector((store) => store.lists);
-  const dispatch = useDispatch();
-  const [moviesList, setMoviesList] = useState(MOVIES);
-  const [filter, setFilter] = useState(false);
-  const [search, setSearch] = useState('');
-
-  const handleSearchMovies = (q) => {
-    setSearch(q);
-  }
-
-  const handleSetList = (listId) => {
-    dispatch(setList(listId));
-  }
-
-  const handleFilterMovies = (f) => {
-    setFilter(f);
-  }
-
-  const handleAddMovie = (movie) =>
-    setMoviesList([{ title: movie, watched: false }, ...moviesList])
-
-  const handleUpdateMovie = (movie, f) => {
-    const list = moviesList.slice();
-    for (var i = 0; i < list.length; i++) {
-      if (list[i].title === movie.title) {
-        list[i]['watched'] = f;
-        setMoviesList(list);
-        return;
-      }
-    }
-  }
-
-  React.useEffect(() => {
-    if (!requestToken || !sessionId)
-      return;
-    dispatch(handleAuthSuccess(requestToken, sessionId));
-    dispatch(handleFetchLists())
-  }, [dispatch]);
-
-  const searchResults =
-    lists && Object.keys(lists).length && list
-      ? lists[list].items.filter(({ title }) =>
-        title.toLowerCase().includes(search.toLowerCase())
-      ) : [];
-
-  const _lists = lists
-    ? Object.values(lists)
-    : [];
+const App = () => {
 
   return (
     <div>
       <h1>Movie List</h1>
 
       <div className="container">
-        <div className="container">
-          <AddMovie />
-        </div>
 
-        <Search onSubmit={handleSearchMovies}>
-          {_lists && _lists.length && (
-            <MovieLists
-              lists={_lists}
-              list={list}
-              onSelect={handleSetList}
-            />
-          )}
-        </Search>
-        <MoviesList movies={searchResults} onToggleMovie={handleUpdateMovie} />
       </div>
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -1,64 +1,7 @@
-import Axios from 'Axios';
-import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from "redux";
-import { Provider } from 'react-redux';
-import { API_READ_KEY } from '/config.js';
-import reducer from './reducers';
-import middleware from './middleware';
 import App from './components/App.jsx';
 
-const store = createStore(
-  reducer,
-  middleware
-);
-
-function fetchRequestToken() {
-  const url = 'https://api.themoviedb.org/3/authentication/token/new';
-  const headers = {
-    Authorization: 'Bearer ' + API_READ_KEY,
-    accept: 'application/json'
-  };
-  Axios.get(url, {
-    headers
-  }).then((response) => {
-    const token = response.data.request_token;
-    window.location.href = 'https://www.themoviedb.org/authenticate/' + token + '?redirect_to=http://localhost:3000';
-  });
-}
-
-function requestSessionId(request_token) {
-  const url = 'https://api.themoviedb.org/3/authentication/session/new';
-  return Axios.post(url, {
-    "request_token": request_token
-  }, {
-    headers: {
-      Authorization: 'Bearer ' + API_READ_KEY,
-      accept: 'application/json',
-      'Content-Type': 'application/json'
-    }
-  }).catch((err) => fetchRequestToken());
-}
-
-async function init(cb) {
-  const params = new URLSearchParams(document.location.search);
-  const requestToken = params.get('request_token');
-  const approved = params.get('approved');
-  if (!requestToken && !approved) {
-    return fetchRequestToken();
-  } else if (requestToken && approved) {
-    const response = await requestSessionId(requestToken);
-    const sessionId = response.data.session_id;
-
-    cb(requestToken, sessionId);
-  }
-}
-
-init((requestToken, sessionId) => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App requestToken={requestToken} sessionId={sessionId} />
-    </Provider>,
-    document.getElementById('app')
-  )
-});
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
